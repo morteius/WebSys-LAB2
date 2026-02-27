5637c6fb85d713a5c92f44a637cb54b43d98ba8f
m <136249187+morteius@users.noreply.github.com>
2026-02-28 01:11:55 +0800

    a

--- scripts/image-list.js
+++ scripts/image-list.js
@@ -1,7 +1,6 @@
-  // full media file names (with extensions)
-
+// AUTO-GENERATED - DO NOT EDIT
 const galleryImages = [
-
-
+  // This file will be automatically updated by GitHub Actions
+  // when you add new images to /images/gallery/
 ];
-console.log('GALLERY LIST LOADED:', galleryImages.length, 'IMAGES');
+console.log('📸 GALLERY LIST LOADED:', galleryImages.length, 'IMAGES');
