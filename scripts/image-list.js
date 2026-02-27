bd2c56511ec1353290157fcb1b1e12d5ffa8ef72
m <136249187+morteius@users.noreply.github.com>
2026-02-28 00:57:51 +0800

    Update image-list.js

--- scripts/image-list.js
+++ scripts/image-list.js
@@ -1,8 +1,6 @@
   // full media file names (with extensions)
 
 const galleryImages = [
-    'day2_photobooth1.jpeg',
-    'day2_photobooth3.jpeg',
 
 ];
 console.log('GALLERY LIST LOADED:', galleryImages.length, 'IMAGES');
